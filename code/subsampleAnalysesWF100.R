fb31a82218e4f6117524e764b0898632c7854465
wmmurrah <william.murrah@gmail.com>
2021-06-29 13:23:30 -0500

    Explore richards curve (not working)

--- code/subsampleAnalysesWF100.R
+++ code/subsampleAnalysesWF100.R
@@ -43,16 +43,23 @@ modlogis.lis <- nlsList(math ~ SSlogis(cagey, Asym, xmid, scal) | childid,
 modlogis.nls <- nls(math ~ SSlogis(cagey, Asym, xmid, scal),
                     data = eclskwf100)
 
+modlogis.nlme <- nlme(math ~ SSlogis(cagey, Asym, xmid, scal),
+                      data = eclskwf100,
+                      fixed = As)
 summary(coef(modlogis.nls))
 modlogis <- nlme(math ~ SSlogis(cagey, Asym, xmid, scal),
                  data = eclskwf100,
-                 fixed = Asym + xmid + scal ~ 1,
+                 fixed = list(Asym ~ 1, xmid ~ 1, scal ~ 1),
                  random = Asym + xmid + scal ~ 1 | childid,
-                 start = c(133, -1.3, 1.5),
+                 start = c(Asym = 133, xmid = -1.3, scal = 1.4),
                  na.action = na.omit)
 summary(modlogis)
+modlogis_scal <- update(modlogis, random = Asym + xmin ~ 1 | childid)
 
+summary(modlogis_xmid)
+anova(modlogis, modlogis_xmid)
 plot(modlogis)
+plot(modlogis, childid ~ resid(.), abline = 0)
 
 modjb.nls <- nls(math ~ b_1i + b_2i*cagey + b_3i*(exp(gamma*cagey)-1),
                  data = eclskwf100,
@@ -70,3 +77,32 @@ modjb <- nlme(math ~ b_1i + b_2i*cagey + b_3i*(exp(b_4i*cagey)-1),
               random = b_1i + b_2i + b_3i + b_4i ~ 1 | childid,
               start = c(133, -1.3, 1.5, 4),
               control = lmeControl(opt='optim', msMaxIter = 2000))
+
+
+library(FlexParamCurve)
+
+mp <- modpar(eclskwf100$cagey, y = eclskwf100$math, )
+modrich.nls <- nls(math ~ SSposnegRichards(cagey, Asym, K, Infl, M, 
+                                           RAsym, Rk, Ri, modno = 12),
+                   data = eclskwf100,
+                   pn.options = mp)
+
+
+library(NRAIA)
+
+modrich.nls <- nls(math ~ richards(cagey, Asym, xmid, scal, lpow),
+                   data = eclskwf100,
+                   start = c(Asym = 133, xmid))
+
+modrich <- nlme(math ~ richards(cagey, Asym, xmid, scal, lpow),
+                data = eclskwf100,
+                fixed = list(Asym ~ x12sesl, xmid ~ 1, scal ~ 1, lpow ~ 1),
+                random = Asym + xmid + scal + lpow ~ 1 | childid,
+                start = c(Asym = 133, xmid = -1.3, scal = 1.5, lpow = 2,
+                          x12sesl = 2),
+                na.action = na.omit)
+
+
+richards <- deriv( ~ Asym/(1 + lpow*exp(-(x - xmid)/scal)),
+                  c("Asym", "xmid", "scal", "lpow"), 
+                  function(x, Asym, xmid, scal, lpow){})
